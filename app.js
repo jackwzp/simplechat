65d0a23a16d5c2cb16a170ef9c7562a78c2587c2
Jack Wu <jackwzp@gmail.com>
2015-12-18 14:22:23 -0800

    initial commit

---
+++ app.js
@@ -0,0 +1,35 @@
+//============================
+//    Initialization
+//============================       
+var express = require('express');
+var path = require('path');
+var app = express();
+
+
+//============================
+//    Middleware
+//============================       
+
+require('_/middleware').config(app);
+require('_/middleware').setLocals(app);
+
+//============================
+//    Routes
+//============================ 
+
+var index = require('_/routes/index');
+var users = require('_/routes/users');
+app.use('/', index);
+app.use('/users', users);
+
+
+//============================
+//    Error Handlers
+//============================   
+require('_/middleware').errorHandler(app);
+
+
+//============================
+//    Expose
+//============================  
+module.exports = app;
